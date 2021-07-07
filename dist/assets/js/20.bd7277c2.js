39b20d31feaffcf479dd467b03b987dcd5c234f0
renwenji <renwenji@jsjd.cc>
2021-07-07 17:33:12 +0800

    master鍐呭鏇存柊

---
+++ dist/assets/js/20.bd7277c2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{386:function(t,e,n){"use strict";n.r(e);var s=n(24),o=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("blockquote",[e("p",[this._v("2021-7-7 试一试！")])])])}),[],!1,null,null,null);e.default=o.exports}}]);
--- update.sh
+++ update.sh
@@ -3,23 +3,36 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
+# 生成静态文件,最终在项目根目录下，生成dist文件夹(很多内容)
 npm run build
 
-# 进入生成的目录文件夹
-cd /dist
+#提交当前master分支的记录
+git add .
+git commit -m 'master内容更新'
+git push origin master
 
-# 复制dist文件夹下的所有文件
-cp /dist/*
-
-# 切换到已有的分支
+# 这里为避免切换错误，删除 .idea/目录
+# 从master分支切换到已有的分支gh-pages
+rm -rf  .idea/
 git checkout gh-pages
 
-# 粘贴刚刚复制的dist文件夹的所有内容到此
-cp /dist/*
+# copy master分支下的dist文件到当前分支gh-pages下
+git checkout master -- dist/
 
-# 提交生成的信息，并进行部署
+
+# 循环将 dist文件夹的里的所有内容粘贴到gh-pages分支，当前项目的根目录上（因为最终网站访问的就是项目文件下的index.html）
+cp -r dist/* ./
+
+
+# 在gh-pages提交生成的信息，并进行部署
 git add .
 git commit -m 'deploy'
 git push origin gh-pages
+
+
+# 完成结束后，再切回主分支，进行编辑
+# 这里为避免切换错误，删除 .idea/目录
+rm -rf  .idea/
+git checkout master
+
 
